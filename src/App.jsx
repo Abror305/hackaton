8eb3567c25fc5bbef3b6104e9db220efeef12643
Yusuf Xojimurodov <xojimurodovyusuf7@gmail.com>
2025-09-14 09:38:35 +0500

    addded JONKA text

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/Login";
 
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <></>
-  )
+    <>
+      <h1>JONKA</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
